2ab7a6b997c517052d74e1fbe3e09ad84b31a69d
Mateusz Ż <zochowski.mateusz@outlook.com>
2022-06-11 12:49:44 +0200

    add fetching dogs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useAppDispatch, useAppSelector } from './app/hooks'
 import { incremented, decremented, reseted } from './features/counter/counterSlice'
+import { useFetchBreedsQuery } from './features/dogs/dogsApiSlice'
 
 import logo from './logo.svg'
 import './App.css'
@@ -8,6 +9,9 @@ import './App.css'
 function App() {
   const count = useAppSelector(state => state.counter.value);
   const dispatch = useAppDispatch();
+
+  const [numDogs, setNumDogs] = useState(10);
+  const { data = [], isFetching } = useFetchBreedsQuery(numDogs);
 
   const handleIncrementClick = () => {
     dispatch(incremented());
@@ -37,6 +41,41 @@ function App() {
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
+
+        <div>
+          <p>Dogs to fetch:</p>
+
+          <select value={numDogs} onChange={(e) => setNumDogs(Number(e.target.value))}>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+          </select>
+        </div>
+
+        <div>
+          Numbers of dogs fetched: {data.length}
+
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Picture</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map(breed => (
+                <tr key={breed.id}>
+                  <td>{breed.name}</td>
+                  <td>
+                    <img src={breed.image.url} alt={breed.name} height={250} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
         <p>
           <a
             className="App-link"
